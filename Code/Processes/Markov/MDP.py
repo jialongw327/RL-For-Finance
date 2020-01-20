eab1e07371e9db2487e43fc3450f0c24873725ea
unknown <jialongw@stanford.edu>
2020-01-20 05:08:34 -0800

    Third Commit

--- Code/Processes/Markov/MDP.py
+++ Code/Processes/Markov/MDP.py
@@ -1,9 +1,11 @@
 import numpy as np
 from MP import MP
 from MRP import MRP
-from typing import Generic
+from typing import Generic, Set
 import sys
 sys.path.append('Code/Utils')
+sys.path.append('Code/Processes/Markov')
+from Markov_Functions import find_all_states
 from Generic_TypeVars import S, A
 from Standard_TypeVars import SSf, STSff, SATSff
 from General_Utils import zip_dict_of_tuple
@@ -14,6 +16,7 @@ class MDP(Generic[S, A]):
     def __init__(self, mdp_graph: SATSff, gamma: float):
         d = {k: zip_dict_of_tuple(v) for k, v in mdp_graph.items()}
         mdp_transition_graph, mdp_reward_graph = zip_dict_of_tuple(d)
+        self.all_states: Set[S] = find_all_states(mdp_graph)
         print(mdp_reward_graph)
 
 
@@ -49,7 +52,10 @@ if __name__ == '__main__':
                     },
             'Facebook':{'FB':({'Facebook': 1}, -1.0), 'Quit':({'C1': 1}, 0.0)
                     },
-            'Sleep':{'SLP':({'Sleep': 1}, 0.0)}
+            'Sleep':{'FB':({'Sleep': 1}, 0.0),
+                     'Study':({'Sleep': 1}, 0.0),
+                     'Pub':({'Sleep': 1}, 0.0),
+                     'SLP':({'Sleep': 1}, 0.0)}
             }
             
-    mdp_obj = MDP(data, 0.95)
+    mdp_obj = MDP(student, 0.95)
